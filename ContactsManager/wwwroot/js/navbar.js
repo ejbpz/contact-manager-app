a45dfede0a81eccdc75b8e85ccb008acb20b90d2
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2025-08-28 16:55:52 -0600

    Adding JavaScript to display menu options (mobile) and keep selected the option in menu (desktop).

---
+++ ContactsManager/wwwroot/js/navbar.js
@@ -0,0 +1,26 @@
+﻿const routes = new Object({
+    0: '/people',
+    1: '/people/new-person'
+});
+
+let buttonMenu = document.querySelector('#buttonMenu');
+let menuItems = document.querySelector('#menuItems');
+let options = document.querySelectorAll('.options');
+
+buttonMenu.addEventListener('click', () => {
+    menuItems.classList.toggle('hidden');
+});
+
+for (let i = 0; i < Object.keys(routes).length; i++) {
+    if (document.location.pathname == routes[i]) {
+        options[i].classList.add('menu-options-selected');
+    } else {
+        options[i].classList.remove('menu-options-selected');
+    }
+}
+
+document.addEventListener('click', () => {
+    if (!menuItems.classList.contains('hidden') && (document.activeElement.id != 'buttonMenu' && document.activeElement.id != 'menuItems')) {
+        menuItems.classList.add('hidden');
+    }
+});
--- ContactsManager/wwwroot/js/toastr-behavior.js
+++ ContactsManager/wwwroot/js/toastr-behavior.js
@@ -3,7 +3,7 @@
     "debug": false,
     "newestOnTop": true,
     "progressBar": false,
-    "positionClass": "toast-top-center",
+    "positionClass": "toast-top-right",
     "preventDuplicates": false,
     "onclick": null,
     "showDuration": "300",
